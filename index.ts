c3ce2c0573a379bea99c1c778c4723d9978f46a4
Wojciech Wieczorek <Wojciech.Wieczorek@contracted.pmi.com>
2022-04-20 15:20:37 +0200

    remove-actions

--- index.ts
+++ index.ts
@@ -4,5 +4,5 @@ import { getKataInRange, sendKata } from "./src/kata-api";
 
 (async () => {
   const kata = await getKataInRange({ start: 4, end: 7 });
-  await sendKata(kata.data);
+  await sendKata(kata);
 })();
